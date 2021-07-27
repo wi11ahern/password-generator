e98d248b68d975c7ace606191a0e77100163fd86
William Ahern <willdahern@gmail.com>
2021-07-27 10:38:17 -0400

    Add unit tests for PasswordGenerator class

---
+++ tests/unit/password_generator_unit_tests.py
@@ -0,0 +1,61 @@
+import string
+
+from unittest import TestCase
+
+from pasword_generator import PasswordGenerator
+
+
+class PasswordGeneratorUnitTests(TestCase):
+
+    def test_generate_password_default(self):
+        """
+        Tests if the generated password is:
+        - A string
+        - 12 characters long
+        - Includes at least 1 special character
+        - Includes at least 1 digit
+        """
+        password_generator = PasswordGenerator()
+        password = password_generator.generate_password()
+
+        self.assertIsInstance(password, str)
+        self.assertTrue(len(password) == 12)
+        self.assertTrue((any(char in '!@#$%&' for char in password)))
+        self.assertTrue((any(char in string.digits for char in password)))
+
+    def test_generate_password_without_special_characters(self):
+        """
+        Tests if the generated password is does NOT include special characters.
+        """
+        password_generator = PasswordGenerator(
+            include_special_chars=False
+        )
+        password = password_generator.generate_password()
+
+        self.assertTrue((not any(char in '!@#$%&' for char in password)))
+
+    def test_generate_password_without_numbers(self):
+        """
+        Tests if the generated password is does NOT numbers.
+        """
+        password_generator = PasswordGenerator(
+            include_numbers=False
+        )
+        password = password_generator.generate_password()
+
+        self.assertTrue((not any(char in string.digits for char in password)))
+
+    def test_generate_password_with_keywords(self):
+        """
+        Tests if the generated password includes keywords.
+        """
+        keywords = ['hello', 'world']
+        password_generator = PasswordGenerator(
+            keywords=keywords
+        )
+        password = password_generator.generate_password()
+
+        self.assertTrue((any(keyword in password for keyword in keywords)))
+
+
+
