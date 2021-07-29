6d585266e16d753cdfd230d05459f92afe0a4ac9
William Ahern <willdahern@gmail.com>
2021-07-29 19:43:12 -0400

    Add GUI option for generating human-readable-passwords

--- password_generator_gui.py
+++ password_generator_gui.py
@@ -9,33 +9,142 @@ class PasswordGeneratorGui:
         body_text_font = ('Aerial', 16)
         frame_title_font_size = ('Aerial', 20)
 
-        password_options_frame_layout = [
-            [sg.Text(text='Password Length', justification='left', font=body_text_font)],
-            [sg.Slider(key='-PASSWORD_LENGTH-', range=(8, 100), default_value=6, size=(30, 10), orientation='horizontal', font=body_text_font)],
-            [sg.Checkbox(key='-DIGITS-', text='Include Numbers', default=True, font=body_text_font)],
-            [sg.Checkbox(key='-SPECIAL_CHARS-', text='Include Special Characters', default=True, font=body_text_font)],
+        password_options_column_one_layout = [
+            [
+                sg.Text(
+                    text='Password Length',
+                    justification='left',
+                    font=body_text_font
+                )
+            ],
+            [
+                sg.Slider(
+                    key='-PASSWORD_LENGTH-',
+                    range=(8, 100),
+                    default_value=6,
+                    size=(30, 10),
+                    orientation='horizontal',
+                    font=body_text_font
+                )
+            ],
+            [
+                sg.Checkbox(
+                    key='-DIGITS-',
+                    text='Include Numbers',
+                    default=True,
+                    font=body_text_font
+                )
+            ],
+            [
+                sg.Checkbox(
+                    key='-SPECIAL_CHARS-',
+                    text='Include Special Characters',
+                    default=True,
+                    font=body_text_font
+                )
+            ],
+        ]
+
+        password_options_column_two_layout = [
+            [
+                sg.Text(
+                    text='Number of Words',
+                    justification='left',
+                    font=body_text_font
+                )
+            ],
+            [
+                sg.Slider(
+                    key='-NUM_OF_WORDS-',
+                    range=(8, 100),
+                    default_value=6,
+                    size=(30, 10),
+                    orientation='horizontal',
+                    font=body_text_font
+                )
+            ],
+        ]
+
+        password_options_layout = [
+            [
+                sg.DropDown(
+                    key='-OPTIONS_LIST-',
+                    values=['Shuffled Passwords', 'Human-Readable Passwords'],
+                    default_value='Shuffled Passwords',
+                    enable_events=True,
+                    font=body_text_font,
+                )
+            ],
+            [
+                sg.Column(
+                    key='-PO_COL_1-',
+                    layout=password_options_column_one_layout
+                ),
+                sg.Column(
+                    key='-PO_COL_2-',
+                    layout=password_options_column_two_layout,
+                    visible=False
+                )
+            ],
         ]
 
         output_frame_layout = [
-            [sg.Output(key='-OUTPUT-', size=(35, 11), font=body_text_font)]
+            [
+                sg.Output(
+                    key='-OUTPUT-',
+                    size=(35, 11),
+                    font=body_text_font
+                )
+            ]
         ]
 
         button_frame_layout = [
             [
-                sg.Button('Generate', key='-GENERATE-', font=body_text_font),
-                sg.Button('Reset', key='-RESET-', font=body_text_font),
-                sg.Button('Clear Output', key='-CLEAR-', font=body_text_font),
-                sg.Button('Exit', key='-EXIT-', font=body_text_font),
+                sg.Button(
+                    button_text='Generate',
+                    key='-GENERATE-',
+                    font=body_text_font
+                ),
+                sg.Button(
+                    button_text='Reset',
+                    key='-RESET-',
+                    font=body_text_font
+                ),
+                sg.Button(
+                    button_text='Clear Output',
+                    key='-CLEAR-',
+                    font=body_text_font
+                ),
+                sg.Button(
+                    button_text='Exit',
+                    key='-EXIT-',
+                    font=body_text_font
+                ),
             ]
         ]
 
         layout = [
             [
-                sg.Frame('Password Options', password_options_frame_layout, element_justification='l', font=frame_title_font_size),
-                sg.Frame('Generated Passwords', output_frame_layout, element_justification='c', font=frame_title_font_size)
+                sg.Frame(
+                    'Password Options',
+                    layout=password_options_layout,
+                    element_justification='c',
+                    font=frame_title_font_size
+                ),
+                sg.Frame(
+                    'Generated Passwords',
+                    layout=output_frame_layout,
+                    element_justification='c',
+                    font=frame_title_font_size
+                )
             ],
             [
-                sg.Frame('', button_frame_layout, element_justification='c', border_width=0)
+                sg.Frame(
+                    title='',
+                    layout=button_frame_layout,
+                    element_justification='c',
+                    border_width=0
+                )
             ]
         ]
 
@@ -49,23 +158,43 @@ class PasswordGeneratorGui:
             element_justification='c',
         ).finalize()
 
+        password_length_key = '-PASSWORD_LENGTH-'
         while True:
             event, values = window.read()
 
             if event == '-GENERATE-':
                 password_generator = PasswordGenerator(
-                    password_length=int(values['-PASSWORD_LENGTH-']),
+                    password_length=int(values[password_length_key]),
                     include_numbers=values['-DIGITS-'],
                     include_special_chars=values['-SPECIAL_CHARS-'],
                 )
-                password = password_generator.generate_password()
+                password = password_generator.shuffled_password()
                 print(password)
                 print('\n')
 
+            if event == '-OPTIONS_LIST-':
+                if values['-OPTIONS_LIST-'] == 'Shuffled Passwords':
+                    password_length_key = '-PASSWORD_LENGTH-'
+                    window['-PO_COL_1-'].update(visible=True)
+                    window['-PO_COL_2-'].update(visible=False)
+
+                elif values['-OPTIONS_LIST-'] == 'Human-Readable Passwords':
+                    password_length_key = '-NUM_OF_WORDS-'
+                    window['-PO_COL_1-'].update(visible=False)
+                    window['-PO_COL_2-'].update(visible=True)
+
             if event == '-RESET-':
-                window.FindElement('-DIGITS-').Update(True)
-                window.FindElement('-SPECIAL_CHARS-').Update(True)
-                window.FindElement('-PASSWORD_LENGTH-').Update(0)
+                if '-DIGITS-' in values.keys():
+                    window.FindElement('-DIGITS-').Update(True)
+
+                if '-SPECIAL_CHARS-' in values.keys():
+                    window.FindElement('-SPECIAL_CHARS-').Update(True)
+
+                if '-PASSWORD_LENGTH-' in values.keys():
+                    window.FindElement('-PASSWORD_LENGTH-').Update(0)
+
+                if '-NUM_OF_WORDS-' in values.keys():
+                    window.FindElement('-NUM_OF_WORDS-').Update(0)
 
             if event == '-CLEAR-':
                 window.FindElement('-OUTPUT-').Update('')
