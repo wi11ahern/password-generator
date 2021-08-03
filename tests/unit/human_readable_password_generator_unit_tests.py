ca8ddd547355e04884c45574dc91b5282acc352e
William Ahern <willdahern@gmail.com>
2021-08-02 20:05:13 -0400

    Extend HumanReadablePasswordGeneratorUnitTests suite with additional tests

--- tests/unit/human_readable_password_generator_unit_tests.py
+++ tests/unit/human_readable_password_generator_unit_tests.py
@@ -1,10 +1,11 @@
+import string
 
 from unittest import TestCase
 
 from src.password_generator.human_readable_password_generator import HumanReadablePasswordGenerator
 
 
-class PasswordGeneratorUnitTests(TestCase):
+class HumanReadablePasswordGeneratorTests(TestCase):
 
     def test_generate_human_readable_password(self):
         """
@@ -20,4 +21,28 @@ class PasswordGeneratorUnitTests(TestCase):
         self.assertIsInstance(password, str)
         self.assertTrue(len(password.split('-')) == 4)
         self.assertTrue(len(password.split('-')) == 4)
+
+    def test_generate_password_with_special_characters(self):
+        """
+        Tests if the generated password does NOT include special characters.
+        """
+        password_generator = HumanReadablePasswordGenerator(
+            include_special_chars=True
+        )
+        password = password_generator.generate_password()
+        print(password)
+
+        self.assertTrue((any(char in password_generator._special_characters for char in password)))
+
+    def test_generate_password_with_numbers(self):
+        """
+        Tests if the generated password does NOT include numbers.
+        """
+        password_generator = HumanReadablePasswordGenerator(
+            include_numbers=True
+        )
+        password = password_generator.generate_password()
+        print(password)
+
+        self.assertTrue((any(char in string.digits for char in password)))
 
